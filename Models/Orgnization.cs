e3b962a05c6e3c8e0ceaa40571e6c6ee30154ab9
AyushSinghSql <ayush.singh@revolvespl.com>
2026-03-20 11:12:22 -0400

    add master backend

---
+++ Models/Orgnization.cs
@@ -0,0 +1,142 @@
+﻿using System.ComponentModel.DataAnnotations;
+using PlanningAPI.Models;
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("organization")]
+public class Organization
+{
+    [Key]
+    [Column("org_id")]
+    [MaxLength(30)]
+    public string OrgId { get; set; }
+
+    [Required]
+    [Column("org_name")]
+    [MaxLength(100)]
+    public string OrgName { get; set; }
+
+    [Column("lvl_no")]
+    public int LvlNo { get; set; }
+
+    // Existing hierarchy names
+    [Column("l1_org_name")] public string L1OrgName { get; set; }
+    [Column("l2_org_name")] public string L2OrgName { get; set; }
+    [Column("l3_org_name")] public string L3OrgName { get; set; }
+    [Column("l4_org_name")] public string L4OrgName { get; set; }
+    [Column("l5_org_name")] public string L5OrgName { get; set; }
+    [Column("l6_org_name")] public string L6OrgName { get; set; }
+    [Column("l7_org_name")] public string L7OrgName { get; set; }
+    [Column("l8_org_name")] public string L8OrgName { get; set; }
+    [Column("l9_org_name")] public string L9OrgName { get; set; }
+
+    // ✅ New columns
+    [Column("org_top_fl")] public string OrgTopFl { get; set; }
+    [Column("taxble_entity_id")] public string TaxbleEntityId { get; set; }
+    [Column("active_fl")] public string ActiveFl { get; set; }
+    [Column("fy_cd_fr")] public string FyCdFr { get; set; }
+    [Column("pd_no_fr")] public int? PdNoFr { get; set; }
+    [Column("fy_cd_to")] public string FyCdTo { get; set; }
+    [Column("pd_no_to")] public int? PdNoTo { get; set; }
+    [Column("ic_trckng_lvl_no")] public int? IcTrckngLvlNo { get; set; }
+    [Column("org_lvls_no")] public int? OrgLvlsNo { get; set; }
+    [Column("org_abbrv_cd")] public string OrgAbbrvCd { get; set; }
+    [Column("modified_by")] public string ModifiedBy { get; set; }
+    [Column("time_stamp")] public DateTime? TimeStamp { get; set; }
+    [Column("company_id")] public string CompanyId { get; set; }
+
+    // Segment IDs
+    [Column("l1_org_seg_id")] public string L1OrgSegId { get; set; }
+    [Column("l2_org_seg_id")] public string L2OrgSegId { get; set; }
+    [Column("l3_org_seg_id")] public string L3OrgSegId { get; set; }
+    [Column("l4_org_seg_id")] public string L4OrgSegId { get; set; }
+    [Column("l5_org_seg_id")] public string L5OrgSegId { get; set; }
+    [Column("l6_org_seg_id")] public string L6OrgSegId { get; set; }
+    [Column("l7_org_seg_id")] public string L7OrgSegId { get; set; }
+    [Column("l8_org_seg_id")] public string L8OrgSegId { get; set; }
+    [Column("l9_org_seg_id")] public string L9OrgSegId { get; set; }
+    [Column("l10_org_seg_id")] public string L10OrgSegId { get; set; }
+
+    // ICR fields
+    [Column("icr_acct_id_fr")] public string IcrAcctIdFr { get; set; }
+    [Column("icr_ref1_id_fr")] public string IcrRef1IdFr { get; set; }
+    [Column("icr_ref2_id_fr")] public string IcrRef2IdFr { get; set; }
+    [Column("icr_acct_id_to")] public string IcrAcctIdTo { get; set; }
+    [Column("icr_ref1_id_to")] public string IcrRef1IdTo { get; set; }
+    [Column("icr_ref2_id_to")] public string IcrRef2IdTo { get; set; }
+
+    [Column("ec_app_proc_cd")] public string EcAppProcCd { get; set; }
+    [Column("rowversion")] public long? Rowversion { get; set; }
+    [Column("tc_org_fl")] public string TcOrgFl { get; set; }
+    [Column("cobra_org")] public string CobraOrg { get; set; }
+    [Column("sft_fl")] public string SftFl { get; set; }
+    [Column("mes_fl")] public string MesFl { get; set; }
+    [Column("tm_org_fl")] public string TmOrgFl { get; set; }
+    public virtual ICollection<PlEmployee> PlEmployees { get; set; } = new List<PlEmployee>();
+
+    public virtual ICollection<PlProject> PlProjects { get; set; } = new List<PlProject>();
+
+    public ICollection<OrgGroupOrgMapping> OrgGroupMappings { get; set; }
+    = new List<OrgGroupOrgMapping>();
+}
+
+public class OrganizationDto
+{
+    public string OrgId { get; set; }
+    public string OrgName { get; set; }
+    public int LvlNo { get; set; }
+}
+public class OrgGroup
+{
+    public int OrgGroupId { get; set; }
+
+    public string OrgGroupCode { get; set; }
+    public string OrgGroupName { get; set; }
+    public string? Description { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public string? CreatedBy { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public string? ModifiedBy { get; set; }
+    public DateTime? ModifiedAt { get; set; }
+
+    // Navigation Properties
+    public ICollection<OrgGroupUserMapping> UserMappings { get; set; }
+        = new List<OrgGroupUserMapping>();
+
+    public ICollection<OrgGroupOrgMapping> OrgMappings { get; set; }
+        = new List<OrgGroupOrgMapping>();
+}
+
+public class OrgGroupOrgMapping
+{
+    public int OrgGroupId { get; set; }
+    public string OrgId { get; set; }
+
+    public OrgGroup OrgGroup { get; set; }
+    public Organization Organization { get; set; }
+}
+
+public class BulkGroupOrgsToggleRequest
+{
+    public int GroupId { get; set; }
+    public List<string> OrgIds { get; set; } = new();
+}
+
+public class BulkUserOrgsToggleRequest
+{
+    public int UserId { get; set; }
+    public List<string> OrgIds { get; set; } = new();
+}
+
+public class OrgGroupCreateUpdateDto
+{
+    public string OrgGroupCode { get; set; } = null!;
+    public string OrgGroupName { get; set; } = null!;
+    public string? Description { get; set; }
+    public bool IsActive { get; set; }
+}
---
+++ Models/PlOrgnization.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PlanningAPI.Models;
+
+public partial class PlOrgnization
+{
+    public string OrgId { get; set; } = null!;
+
+    public string OrgName { get; set; } = null!;
+
+    [Column("org_top_fl")]
+    [MaxLength(1)]
+    public string OrgTopFl { get; set; }
+
+    public decimal IcTrckngLvlNo { get; set; }
+
+    public decimal OrgLvlsNo { get; set; }
+
+    public string OrgAbbrvCd { get; set; } = null!;
+
+    [Column("lvl_no")]
+    public int LvlNo { get; set; }
+
+    [Column("taxble_entity_id")]
+    [MaxLength(10)]
+    public string TaxbleEntityId { get; set; }
+
+
+    [Column("active_fl")]
+    [MaxLength(1)]
+    public string ActiveFl { get; set; }
+
+    [Column("fy_cd_fr")]
+    [MaxLength(6)]
+    public string? FyCdFr { get; set; }
+
+    [Column("pd_no_fr")]
+    public int? PdNoFr { get; set; }
+
+    [Column("fy_cd_to")]
+    [MaxLength(6)]
+    public string? FyCdTo { get; set; }
+
+    [Column("pd_no_to")]
+    public int? PdNoTo { get; set; }
+
+    public string ModifiedBy { get; set; } = null!;
+
+    public DateTime TimeStamp { get; set; }
+
+    public string CompanyId { get; set; } = null!;
+
+    //public virtual ICollection<EmployeeMaster> PlEmployees { get; set; } = new List<EmployeeMaster>();
+
+    public virtual ICollection<PlProject> PlProjects { get; set; } = new List<PlProject>();
+}
